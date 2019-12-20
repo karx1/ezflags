0618874625cfab42f52dc8b45608d7eaa7892416
nerdstep710 <nerdstep710@gmail.com>
2019-12-20 16:42:47 -0600

    Reformat code

--- ezflags/flagparser.py
+++ ezflags/flagparser.py
@@ -9,11 +9,10 @@ def _parse_bool(inp: bool):
 
 
 class FlagParser(argparse.ArgumentParser):
-
     def add_flag(self, *args: str, action: bool, help: str = None):
-        action_str = f'store_{_parse_bool(action)}'
+        action_str = f"store_{_parse_bool(action)}"
         self.add_argument(*args, action=action_str, help=help)
-    
+
     def parse_flags(self):
         args = self.parse_args()
         return args
--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup
 import ezflags
 
-readme = ''
-with open('README.rst') as f:
+readme = ""
+with open("README.rst") as f:
     readme = f.read()
 
 
 setup(
-    name='ezflags',
+    name="ezflags",
     version=ezflags.__version__,
-    packages=['ezflags'],
-    url='https://github.com/karx1/ezflags',
-    license='MIT',
-    author='karx',
-    author_email='nerdstep710@gmail.com',
-    description='A tool that makes creating command line flags super easy.',
+    packages=["ezflags"],
+    url="https://github.com/karx1/ezflags",
+    license="MIT",
+    author="karx",
+    author_email="nerdstep710@gmail.com",
+    description="A tool that makes creating command line flags super easy.",
     long_description=readme,
-    python_requires='>=3.2',
+    python_requires=">=3.2",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-    ]
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+    ],
 )
