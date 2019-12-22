1447c67b399ae58f0b6689cdb7521c87a75138a7
nerdstep710 <nerdstep710@gmail.com>
2019-12-21 19:43:19 -0600

    Add extras_require section to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ readme = ""
 with open("README.rst") as f:
     readme = f.read()
 
+extras_require = {
+    'docs': [
+        'sphinx',
+        'sphinx-rtd-theme'
+    ]
+}
 
 setup(
     name="ezflags",
@@ -17,6 +23,7 @@ setup(
     description="A tool that makes creating command line flags super easy.",
     long_description=readme,
     python_requires=">=3.2",
+    extras_require=extras_require,
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
