b6bb4b74d3ba36c2bf7ab562a0bb7a554e5f8129
nerdstep710 <nerdstep710@gmail.com>
2020-01-03 12:33:45 -0600

    Limit args to two values and improve type checking. Also add required kwarg

--- ezflags/flagparser.py
+++ ezflags/flagparser.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+from typing import List
 
 
 def _parse_bool(inp: bool):
@@ -14,20 +15,23 @@ class FlagParser(argparse.ArgumentParser):
     This is the main class for parsing flags.
     It extends :class:`argparse.ArgumentParser`, and uses the same parameters for __init__.
     """
-    def add_flag(self, *args: str, value: bool, help: str = None):
+    def add_flag(self, *args: str, value: bool, help: str = None, required: bool = False):
         """Add a flag to the parser.
 
-        :param args: Things to name the flag.
+        :param args: Things to name the flag. Maximum of two values.
         :type args: str
         :param value: The value of the flag when present.
         :type value: bool
-        :param help: The help message to display when invoked with -h or --help.
+        :param required: Whether the flag is required for the script to run. Defaults to False.
+        :type required: bool, optional
+        :param help: A brief description of the flag. These descriptions will be displayed when the `-h` or `--help` flags are present.
         :type help: str, optional
         """
+        args = args[:2]
         action_str = "store_{}".format(_parse_bool(value))
-        self.add_argument(*args, action=action_str, help=help)
+        self.add_argument(*args, action=action_str, help=help, required=required)
 
-    def parse_flags(self, flag_list=None):
+    def parse_flags(self, flag_list: List[str] = None) -> argparse.Namespace:
         """Parse the flag inputs. Returns an :class:`argparse.Namespace` object with each flag.
         
         :param flag_list: List of flags to parse. This can be used for testing. Defaults to :class:`sys.argv[1:]`.
